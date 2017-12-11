c7e48a388672f7a0a54cfbb71c79080b747d922c
huanghlun <564945308@qq.com>
2017-12-11 15:47:20 +0800

    ....

--- src/com/finalItem/filter/LoginFilter.java
+++ src/com/finalItem/filter/LoginFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -35,6 +36,18 @@ public class LoginFilter implements Filter{
 				System.out.println("登录成功");
 			}
 		}	
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
